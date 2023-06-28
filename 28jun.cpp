1e408b6002e49a207752b3d8b1efaf0d816cea65
Abhishek <96220514+AbhiSince2k@users.noreply.github.com>
2023-06-28 09:28:34 +0530

    Add files via upload

---
+++ 28jun.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+template <typename data>
+
+void printData(data value)
+{
+    cout<<"value="<<value<<endl;
+}
+int main()
+{
+    printData("My Data"); //String
+    printData('a'); // char
+    printData(108); //int 
+    printData(108.8f); //float
+    printData(100.01); //double
+
+    return 0;
+}
